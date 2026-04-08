3802051fb0d9cb99f631e9c7ccab8e9dc8c3cbd6
seoil <seoil@BY211-G-38>
2026-04-08 20:05:10 +0900

    A올립니다

--- src/main/java/teamtest/service/CalculatorService.java
+++ src/main/java/teamtest/service/CalculatorService.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Service;
 public class CalculatorService {
 
     public int add(int a, int b) { return a + b; }
+    
+    public int subtract(int a, int b) {return a - b; }
 }
